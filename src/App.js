f5c05ffcb718f02e7505fd74b2bc6562fd9129b8
Jared Gutkin <jgutkinsam@gmail.com>
2022-08-27 11:06:10 -0500

    static sidebar and styling added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [animeList, setAnimeList] = useState([])
+  const [topAnime, setTopAnime] = useState([])
+  const [search, setSearch] = ("")
   return (
     <div className="App">
       <Header />
+      <div className="content-wrap">
+        <Sidebar />
+      </div>
 
 
     </div>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+function Sidebar() {
+  return (
+    <aside>
+        <nav>
+            <h3>Top Anime</h3>
+            <a 
+                href="#"
+                target="_blank"
+                rel="noreferrer">
+                Chainsaw Man
+            </a>
+            <a 
+                href="#"
+                target="_blank"
+                rel="noreferrer">
+                Food Wars!
+            </a>
+            <a 
+                href="#"
+                target="_blank"
+                rel="noreferrer">
+                Sailor Moon 
+            </a>
+        </nav>
+    </aside>
+  )
+}
+
+export default Sidebar
