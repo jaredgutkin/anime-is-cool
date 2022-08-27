1de22bfd4cc3e69cb4cfeb0b6f8f49b2c18b80cf
Jared Gutkin <jgutkinsam@gmail.com>
2022-08-27 11:49:40 -0500

    trying to figure out how to get this jikan v4 to work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   const [animeList, setAnimeList] = useState([])
   const [topAnime, setTopAnime] = useState([])
   const [search, setSearch] = ("")
+
+  const getTopAnime = async () => {
+    const temp = await fetch(`https://api.jikan.moe/v4/top/anime`)
+      .then(res => res.json())
+
+    setTopAnime(temp.top.slice(0, 2))
+  }
+
+  useEffect(() => {
+      getTopAnime()
+
+  }, [])
+
+  console.log(topAnime)
   return (
     <div className="App">
       <Header />
       <div className="content-wrap">
         <Sidebar />
+        <Main />
       </div>
 
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+function Main() {
+  return (
+    <main>
+        <div className="main-head">
+            <form className="search-box">
+                <input type="search" placeholder="Search for an anime..." required />
+            </form>
+        </div>
+    </main>
+  )
+}
+
+export default Main
